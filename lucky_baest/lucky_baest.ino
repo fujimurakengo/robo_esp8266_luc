4ce38563fc3474193f69be2afe6032dbc2ce8124
fujimurakengo <naruawaryousuke@gmail.com>
2017-09-20 20:57:53 +0900

    コメントを追加

--- lucky_baest/lucky_baest.ino
+++ lucky_baest/lucky_baest.ino
@@ -5,7 +5,7 @@ AquesTalk atp; //インスタンス定義 変数名は任意
 int prevt = 0; 
 int times = 0; 
 int threshold = 5; 
-
+//////////////////////////////////////////////////////////////////////////////////////////
 void setup(){ 
 
   Wire.begin(); 
@@ -16,9 +16,9 @@ void setup(){
      pinMode(8,OUTPUT); 
   pinMode(9,INPUT); 
   pinMode(13,OUTPUT); 
-
-
 } 
+//////////////////////////////////////////////////////////////////////////////////////////
+
 float h,t =0; 
 int times_dalays=100; 
 char jikosyoukai[]={"hajime,ma'_shite","bo'kuwa,/rakki-bi'-_suto,dayo.","kimiwa,/kokoni,/ki'ta.","<NUMK VAL=" + String(h) + " COUNTER=ninnme> ","no,/fure'nnzu,dayo.","kimiwa,/na'niga,/shirita'i?"};//6
@@ -26,28 +26,34 @@ char annnai[]={"kimiwa,/na'niga,/shirita'i?","o'nndo,to,/_shitsu'do?","pimu,ni,t
 char orei[]={"waka'ttayo.","pimu,no,moyoo'shio","yukkuritanoshi'nndene?"}
 char freez[]={"awawawa'wawa wa wa wa wa wa wa "}
 //////////////////////////////
+
+///////////////////自己紹介だよ
 void loop() {
   while(tuch==0){}
   for(int i=0;i<6;i++){
     speak(jikosyoukai[i]);
+ }///////////////////自己紹介だよ
+
  
- }
  int res=0;
+ /////////////////////////////質問だよ
   for(int i=0;i<3;i++){
     for(int jj=0;jj<4;jj++){
     res=question(annnai[jj]);
     if(res>0) break;
     }
   if(res>0) break;
- }  
- if(res>0){/////////////////////////////////////////////////////////////////////////////////////////
+ }
+ /////////////////////////////質問だよ
+   
+ if(res>0){/////////////////////////////////////////////////////////////////////////////////////////通常対応の選択
 
-  } 
+  }//////////////////////////////////////////////////////////////////////////////////////////////// 
  else{
     for(int i=0;i<3;i++){
     speak(orei[i]);
     }
-  }/////////////////////////////////////////////////////////////////////////////////////////
+  }/////////////////////////////////////////////////////////////////////////////////////////無視された時の対応
  
  t =   get_temper();
     
